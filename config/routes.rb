a7ce60f3661a5fe5ce3355e083b672cfa184574d
Paul <paul@pogodan.com>
2014-05-31 17:40:46 -0400

    random stuff

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,9 @@ Rails.application.routes.draw do
     get "/#{name}",     to: "pages##{name}"
   end
   
-  match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
-  match 'auth/failure', to: redirect('/'), via: [:get, :post]
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+  # match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  # match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  # match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   # You can have the root of your site routed with "root"
 end
