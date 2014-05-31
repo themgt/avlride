c3140b09c7988f73903577251b1809fc137015ed
Paul <paul@pogodan.com>
2014-05-31 15:43:02 -0400

    get ride creation working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_filter :authenticate_user!
+  
   protect_from_forgery with: :exception
 
   private
@@ -11,9 +11,4 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
   	'/users/sign_in'
   end
-  
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
 end
--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,4 +6,19 @@ class RidesController < ApplicationController
   def new
     @ride = Ride.new
   end
+  
+  def show
+    @ride = Ride.find(params[:id])
+  end
+  
+  def create
+    raise 'woot'
+    @ride = current_user.rides.create(permitted_params)
+    redirect_to @ride
+  end
+  
+  protected
+  def permitted_params
+    params[:ride].permit(:lat_a, :lat_b, :lon_a, :lon_b, :has_car, :leave_at, :return_at, :message)
+  end
 end
